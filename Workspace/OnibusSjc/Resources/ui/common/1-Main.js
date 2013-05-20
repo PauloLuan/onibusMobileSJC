33dfa53f480fab44ec6e7caa8dc9a4732395f962
Paulo Luan <paulonfsu2@hotmail.com>
2013-05-20 15:26:28 -0300

    adicionado algoritmo de exibição dos próximos 5 horários das linhas favoritas

--- Workspace/OnibusSjc/Resources/ui/common/1-Main.js
+++ Workspace/OnibusSjc/Resources/ui/common/1-Main.js
@@ -15,6 +15,34 @@ function Main() {
 		thisObject.view.add(label);
 		return thisObject.view;
 	}
+	
+	this.showNextBusFavorites = function(){
+		var allTimes = ["09:01", "09:10", "09:15", "09:35", "10:05", "10:35", "10:45", "10:50", "11:01", "11:04", "11:05", "11:09"];
+		var time = "10:35";
+		var count = 0;
+		var nextTimes = [];
+		
+		for (var i=0; i < allTimes.length; i++) {
+			var tempTime = allTimes[i];
+			
+			var arrayHour = tempTime.substring(0, 2);
+			var arrayMinute = tempTime.substring(3, 5);
+			
+			//var currentHour = time.getHours();
+			//var currentMinutes = time.getMinutes()
+			
+			var currentHour = 10;
+			var currentMinute = 35;
+			
+			if(arrayHour >= currentHour && arrayMinute >= currentMinute || arrayHour >= currentHour && count < 5)
+			{
+				count ++;
+				nextTimes.push(arrayHour + ":" + arrayMinute);
+			}
+		};
+		//console.log(nextTimes);
+	}
+	
 	return thisObject;
 }
 
--- Workspace/OnibusSjc/Resources/ui/common/3-DetailBusTime.js
+++ Workspace/OnibusSjc/Resources/ui/common/3-DetailBusTime.js
@@ -12,45 +12,48 @@ function DetailBusTime() {
 		return thisObject.view;
 	}
 
-	this.createScreenBusTime = function(bus){
+	this.createScreenBusTime = function(bus) {
 		/*var contentView = Ti.UI.createView({
-			backgroundColor : 'white',
-			borderWidth : 1,
-			layout : 'vertical',
-			height : 'auto',
-			top : '10dp',
-			right : '10dp',
-			left : '10dp',
-			bottom : '10dp'
-		});*/
+		 backgroundColor : 'white',
+		 borderWidth : 1,
+		 layout : 'vertical',
+		 height : 'auto',
+		 top : '10dp',
+		 right : '10dp',
+		 left : '10dp',
+		 bottom : '10dp'
+		 });*/
 
 		var tipos = ["semana", "sabado", "domingo"]
 		var periodos = ["madrugada", "manha", "tarde", "noite"];
 		var periodos_normalizados = ["\nMadrugada: ", "\nManhã: ", "\nTarde: ", "\nNoite: "];
 		var strHorarios = ["", "", ""];
 
-		for (var i=0; i < tipos.length; i++) {
+		for (var i = 0; i < tipos.length; i++) {
 			strHorarios[i] = "";
-			var tipo_do_dia = bus.horarios[tipos[i]] // array de horários: "semana", "sabado", "domingo"
+			var tipo_do_dia = bus.horarios[tipos[i]]// array de horários: "semana", "sabado", "domingo"
 
-			for (var j=0; j < periodos.length; j++) {
-				strHorarios[i] = strHorarios[i] + periodos_normalizados[j]; // Pega os nomes normalizados para a apresentação para o usuário final "Manhã", "Madrugada"
-				array_horarios = tipo_do_dia[periodos[j]]
+			for (var j = 0; j < periodos.length; j++) {
+				strHorarios[i] = strHorarios[i] + periodos_normalizados[j];
+				// Pega os nomes normalizados para a apresentação para o usuário final "Manhã", "Madrugada"
+				array_horarios = tipo_do_dia[periodos[j]];
 
-				for (var k=0; k < array_horarios.length; k++) { // laço que percorre todos os elementos da lista de horários de cada um dos períodos do dia
-					horario_temp = array_horarios[k]
-					strHorarios[i] = strHorarios[i] + horario_temp
+				if (array_horarios) {
+					for (var k = 0; k < array_horarios.length; k++) {// laço que percorre todos os elementos da lista de horários de cada um dos períodos do dia
+						horario_temp = array_horarios[k]
+						strHorarios[i] = strHorarios[i] + horario_temp
 
-					if(Ti.App.isNumber(horario_temp[0]) && k != tipo_do_dia.length){ // Se o primeiro carácter for número, deve-se colocar a vírgula para separar os horários, senão, deve-se colocar um espaço para as palavras não grudarem
-						strHorarios[i] = strHorarios[i] + ", "
-					} else {
-						strHorarios[i] = strHorarios[i] + " "
+						if (Ti.App.isNumber(horario_temp[0]) && k != tipo_do_dia.length) {// Se o primeiro carácter for número, deve-se colocar a vírgula para separar os horários, senão, deve-se colocar um espaço para as palavras não grudarem
+							strHorarios[i] = strHorarios[i] + ", "
+						} else {
+							strHorarios[i] = strHorarios[i] + " "
+						}
 					}
 				}
 			}
 		}
 
-		var lbl1 = Ti.App.createNormalLabel('Linha: ' + bus.numero + " (" + bus.nome + ")"  );
+		var lbl1 = Ti.App.createNormalLabel('Linha: ' + bus.numero + " (" + bus.nome + ")");
 		var lbl2 = Ti.App.createNormalLabel('Sentido: ' + bus.sentido);
 		var lbl3 = Ti.App.createNormalLabel('Horários');
 		var lbl4 = Ti.App.createNormalLabel('Dias de Semana: ' + strHorarios[0] + "\n");
@@ -58,10 +61,10 @@ function DetailBusTime() {
 		var lbl6 = Ti.App.createNormalLabel('Domingos e Feriados: ' + strHorarios[2] + "\n");
 
 		/*contentView.add(lbl1);
-		contentView.add(lbl2);
-		contentView.add(lbl3);
-		contentView.add(lbl4);
-		contentView.add(lbl5);*/
+		 contentView.add(lbl2);
+		 contentView.add(lbl3);
+		 contentView.add(lbl4);
+		 contentView.add(lbl5);*/
 
 		thisObject.view.add(lbl1);
 		thisObject.view.add(lbl2);
@@ -76,4 +79,4 @@ function DetailBusTime() {
 	return thisObject;
 }
 
-module.exports = DetailBusTime;
+module.exports = DetailBusTime; 
